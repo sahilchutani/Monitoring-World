38169041b6669ebe7af3a694da23b8a4c37411da
Sahil Chutani <sahilchutani123@gmail.com>
2021-12-05 00:52:11 +0530

    Add files via upload

---
+++ monitorin_world.py
@@ -0,0 +1,74 @@
+#from keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import img_to_array
+import imutils
+import cv2
+from tensorflow.keras.models import load_model
+import numpy as np
+import subprocess
+import time
+
+detection_model_path = 'haarcascade_files/haarcascade_frontalface_default.xml'
+emotion_model_path = 'models/final_model.hdf5'
+
+face_detection = cv2.CascadeClassifier(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+EMOTIONS = ["sad" ,"disgust","scared", "happy", "cry", "surprised",
+ "neutral"]
+
+
+# starting video streaming
+cv2.namedWindow('MONITERING WORLD')
+#camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture('/home/pi/Desktop/aml_projext/baby2.mp4')
+x=0
+while True:
+    while x<30:
+
+        frame = camera.read()[1]
+        frame = imutils.resize(frame,width=300)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_detection.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
+
+        canvas = np.zeros((250, 300, 3), dtype="uint8")
+        frameClone = frame.copy()
+        if len(faces) > 0:
+            faces = sorted(faces, reverse=True,
+            key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+            (fX, fY, fW, fH) = faces
+            roi = gray[fY:fY + fH, fX:fX + fW]
+            roi = cv2.resize(roi, (64, 64))
+            roi = roi.astype("float") / 255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+
+
+            preds = emotion_classifier.predict(roi)[0]
+            emotion_probability = np.max(preds)
+            label = EMOTIONS[preds.argmax()]
+            print(label)
+        else: continue
+
+
+        for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
+                    text = "{}: {:.2f}%".format(emotion, prob * 100)
+                    w = int(prob * 300)
+                    cv2.rectangle(canvas, (7, (i * 35) + 5),
+                    (w, (i * 35) + 35), (0, 0, 255), -1)
+                    cv2.putText(canvas, text, (10, (i * 35) + 23),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45,
+                    (255, 255, 255), 2)
+                    cv2.putText(frameClone, label, (fX, fY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+                    cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH),
+                                  (0, 0, 255), 2)
+
+
+        cv2.imshow('MONITERING WORLD', frameClone)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        x+=1
+
+    x=0
+
+camera.release()
+cv2.destroyAllWindows()
